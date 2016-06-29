94215daf9ee9f00709ac1815561493f519bc5bf4
programadormd <jbryan.699@gmail.com>
2016-06-28 18:24:45 -0600

    Cree una clase para crear nuestras tablas en SQLite. Aun en mejora

--- app/src/main/java/gt/lea/usaid/perfiladorlinguistico/utils/CreateTable.java
+++ app/src/main/java/gt/lea/usaid/perfiladorlinguistico/utils/CreateTable.java
@@ -4,53 +4,91 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.provider.BaseColumns;
 
+import java.lang.reflect.Array;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import gt.lea.usaid.perfiladorlinguistico.utils.ConnectivityDataBase;
 
 /**
- * Created by programadormd on 06-24-16.
+ * Created by bryan on 06-24-16.
  */
 public class CreateTable {
 
+    private String nombre_tabla = "";
     private ArrayList<String> valor = new ArrayList();
-    private String nombre_tabla;
+    private ArrayList<String> data = new ArrayList();
+    private ConnectivityDataBase cdb = null;
+    //public CreateTable().SQL().CreateTable("",0,0);
 
-    public CreateTable(String nombre_tabla){
-        this.nombre_tabla = nombre_tabla;
+    private void add(String campo_completo){
+        valor.add(campo_completo);
     }
 
-    public CreateTable(String nombre_tabla, ArrayList<String> valor){
-        this.nombre_tabla = nombre_tabla;
-        this.valor = valor;
-    }
-
-    public void add(String campo){
-        valor.add(campo);
-    }
-
-    public String camp(String llave_primaria,String type_data){
-        String resultado = "CREATE TABLE " + nombre_tabla + "(";
-        resultado +=   " " + "PRIMARY KEY AUTOINCREMENT";
-        if(valor != null || valor.size()  > 0 ){
-            for(int v = 0; v < valor.size(); v ++){
-                if((v + 1) != valor.size()){
-                    resultado += "'" + valor.get(v) + "'" + ",";
-                }else
-                    resultado += "'" + valor.get(v) + "'";
-            }
+    public void add(String campo, int tipo, int dimension, boolean not_null){
+        String dato = campo;
+        switch (tipo){
+            case 1:
+                dato += " VARCHAR(" + dimension + ") ";
+                break;
+            case 2:
+                dato += " INT(" + dimension + ") ";
+            case 3:
+                dato += " CHAR ";
+            case 4:
+                dato += " DATE ";
+            case 5:
+                dato += " DATETIME ";
         }
-        resultado += ");";
-        //resultado = "CREATE TABLE + nombre_tabla + "(" + llave_primaria + cantidad_campos + ");";
-        return resultado;
+        if(not_null == true)
+            dato += "NOT NULL";
+        else
+            dato += "NULL";
+        data.add(dato);
     }
 
-    public static class Colum{
-        public static final String Id_pregunta = BaseColumns._ID;
-        public static final String PREGUNTA = "";
-        public static final int RESULTADO_FINAL = 0;
+    public CreateTable(String nombre_tabla, SQL sql){
+        nombre_tabla += sql.key;
     }
 
+    public void create() throws SQLException{
+
+    }
+
+
+
+    public class SQL {
+
+        private String key;
+
+        public final String primary_key(){
+            return key;
+        }
+
+        public final void setKey(String campo, int dato, int dimension){
+            key = campo + " " ;
+            String data = "";
+            switch (dato){
+                case 1:
+                    data = "VARCHAR(" + dimension + ")";
+                    break;
+                case 2:
+                    data = "INT(" + dimension + ")";
+                case 3:
+                    data = "TEXT(" + dimension + ")";
+                case 4:
+                    data = "DATE";
+                    break;
+                case 5:
+                    data = "DATETIME";
+                    break;
+                case 6:
+                    data = "CHAR";
+
+            }
+            key += data + " PRIMARY KEY NOT NULL," ;
+        }
+    }
 
 
 }

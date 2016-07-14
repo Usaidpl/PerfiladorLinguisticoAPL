5dd07cdfbc73a98b1b6c1ae6c678fa84911d6819
MinZoo <intecapkomgit6@gmail.com>
2016-07-14 17:02:31 -0600

    reform3

--- app/src/main/java/gt/lea/usaid/perfiladorlinguistico/view/Splash.java
+++ app/src/main/java/gt/lea/usaid/perfiladorlinguistico/view/Splash.java
@@ -25,7 +25,7 @@ public class Splash extends Activity {
         Thread timerTread = new Thread(){
             public void run(){
                 try{
-                    sleep(2000);
+                    sleep(1000);
                 }
                 catch (InterruptedException e){
                     e.printStackTrace();

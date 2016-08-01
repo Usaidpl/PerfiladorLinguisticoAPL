a04417328ed5b36999c3c9afe1c7acd6fc597f9a
Intecprom-PC <proyectousaidpl@gmail.com>
2016-08-01 00:17:00 -0500

    models01082016

--- app/src/main/java/gt/lea/usaid/perfiladorlinguistico/Entrevistas.java
+++ app/src/main/java/gt/lea/usaid/perfiladorlinguistico/Entrevistas.java
@@ -1,14 +1,84 @@
 package gt.lea.usaid.perfiladorlinguistico;
 
-import android.app.Activity;
-import android.os.Bundle;
 
-public class Entrevistas extends Activity {
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import gt.lea.usaid.perfiladorlinguistico.models.BaseHelper;
+
+public class Entrevistas extends AppCompatActivity {
+
+    EditText  et_nombre, et_cargo, et_fecha, et_escuela, et_codigo, et_municipio, et_departamento, et_ciclo ;
+    Button  bt_mostrar, bt_guardar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entrevistas);
 
+        et_nombre    =(EditText) findViewById(R.id.et_nombre);
+        et_cargo     =(EditText) findViewById(R.id.et_cargo);
+        et_fecha     =(EditText) findViewById(R.id.et_fecha);
+        et_escuela   =(EditText) findViewById(R.id.et_escuela);
+        et_codigo    =(EditText) findViewById(R.id.et_codigo);
+        et_municipio =(EditText) findViewById(R.id.et_municipio);
+        et_departamento =(EditText) findViewById(R.id.et_departamento);
+        et_ciclo     =(EditText) findViewById(R.id.et_ciclo);
+
+        bt_guardar =(Button) findViewById(R.id.bt_guardar);
+        bt_mostrar = (Button)findViewById(R.id.bt_mostrar);
+
+        bt_guardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                guardar(et_nombre.getText().toString(), et_cargo.getText().toString(), et_fecha.getText().toString(), et_escuela.getText().toString(),
+                        et_codigo.getText().toString(), et_municipio.getText().toString(), et_departamento.getText().toString(),et_ciclo.getText().toString());
+            }
+        });
+        bt_mostrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Entrevistas.this, Listado.class) );
+            }
+        });
+
+    }
+
+    private void guardar(String Nombre, String Cargo, String Fecha,String Escuela, String Codigo,
+                         String Municipio, String Departamento, String Ciclo){
+
+        BaseHelper helper = new BaseHelper(this, "Perfilador",null,1);
+        SQLiteDatabase db = helper.getWritableDatabase();
+
+        try {
+            ContentValues c= new ContentValues();
+            c.put("Nombre", Nombre);
+            c.put("Cargo", Cargo);
+            c.put("Fecha", Fecha);
+            c.put("Escuela",Escuela);
+            c.put("Codigo", Codigo);
+            c.put("Municipio", Municipio);
+            c.put("Departamento", Departamento);
+            c.put("Ciclo", Ciclo);
+
+
+            db.insert("entrevistador", null, c);
+            db.close();
+            Toast.makeText(this,"Registro insertado",Toast.LENGTH_SHORT).show();
+
+
+        }catch (Exception e ){
+
+            Toast.makeText(this,"Error: " + e.getMessage(),Toast.LENGTH_SHORT).show();
+
+        }
     }
 }
+
---
+++ app/src/main/java/gt/lea/usaid/perfiladorlinguistico/models/BaseHelper.java
@@ -0,0 +1,48 @@
+package gt.lea.usaid.perfiladorlinguistico.models;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Intecprom-PC on 31/07/2016.
+ */
+public class BaseHelper  extends SQLiteOpenHelper {
+
+    String tabla_perfil = "CREATE TABLE entrevistador (Cod_entrevistador INTEGER PRIMARY KEY, Nombre TEXT, Cargo TEXT, Fecha DATE," +
+            "Escuela TEXT, Codigo TEXT, Municipio TEXT, Departamento TEXT, Ciclo TEXT)";
+
+    String tabla_estudiante = "CREATE TABLE estudiante(Cod_estudiante INTEGER PRIMARY KEY, Nombre TEXT, Genero TEXT, Edad TEXT, Grado TEXT, entrevistador INTEGER,  FOREIGN KEY(entrevistador) REFERENCES perfil(cod_entrevistador))" ;
+
+
+
+    public BaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("PRAGMA foreign_keys=ON");
+        db.execSQL(tabla_perfil);
+        db.execSQL(tabla_estudiante);
+
+
+    }
+
+
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table_perifl");
+        db.execSQL("drop table_estudiante");
+        db.execSQL(tabla_perfil);
+        db.execSQL(tabla_estudiante);
+
+    }
+
+
+
+
+
+}
